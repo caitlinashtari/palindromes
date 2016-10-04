c939fd5b99ec466db6060f9ab57b6df83b72e314
Caitlin Ashtari <caitlinashtari@gmail.com>
2016-10-03 20:45:24 -0700

    make it functional but not using for loop...

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,28 @@
 /* Back-end --------------*/
-var palindrome = function(word){
-  return false;
-};
+var palindrome;
+var word;
+
+for(i = 0; i <= palindrome.length; i++){
+  if
+}
 
 
 
-
+// var palindrome = function(input){
+//   if (input === input.split('').reverse().join('')) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
 
 
 /* Front-end --------------*/
 $(document).ready(function(){
-
+  $("form#palindrome").submit(function(event){
+    event.preventDefault();
+    var word = $("input#word").val();
+    var result = palindrome(word);
+    $("#result").text(result);
+  });
 });
